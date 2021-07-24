0ce3a11909c2e3157d6f2c643f29205179738e83
A-amon <64846892+A-amon@users.noreply.github.com>
2021-07-24 12:45:30 +0800

    Setup redux debugger

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,14 @@ import { createStore, applyMiddleware } from 'redux'
 import reducer from './index'
 import thunk from 'redux-thunk'
 
+const middlewares = [thunk]
+
+if (__DEV__) {
+    const createDebugger = require('redux-flipper').default
+    middlewares.push(createDebugger())
+}
+
 export default store = createStore(
     reducer,
-    applyMiddleware(thunk)
+    applyMiddleware(...middlewares)
 )
