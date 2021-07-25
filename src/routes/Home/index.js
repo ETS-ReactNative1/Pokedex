0d5f4ae90c26400c6521b3c3f8a82289d28369af
A-amon <64846892+A-amon@users.noreply.github.com>
2021-07-25 11:42:13 +0800

    Added Animated Flatlist

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -1,16 +1,19 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
+import Animated from 'react-native-reanimated'
 import { useSelector, useDispatch } from 'react-redux'
-import { ScrollView } from 'react-native'
 import PropTypes from 'prop-types'
+import { FlatList } from 'react-native'
 import { Pokemon } from '@routes'
-import Searchbar from '@components/Searchbar'
-import PokemonCard from '@components/PokemonCard'
 import { getAllPokemons } from '@redux/actions/pokemon'
-import { StyledHome, Title, Description, styles } from './styles'
+import { StyledHome, Title, Description, StyledSearchbar, StyledPokemonCard } from './styles'
 
 const Home = ({ navigation }) => {
     const dispatch = useDispatch()
     const pokemons = useSelector((state) => state.pokemon.items)
+
+    const scrollY = useRef(new Animated.Value(0)).current
+
+    const AnimatedFlatList = Animated.createAnimatedComponent(FlatList)
 
     useEffect(() => {
         dispatch(getAllPokemons())
@@ -24,11 +27,43 @@ const Home = ({ navigation }) => {
             <Description>
                 Search for Pokemon by name or using the National Pokedex number.
             </Description>
-            <Searchbar style={styles.searchbar} />
-            <ScrollView>
+            <StyledSearchbar />
+            <AnimatedFlatList
+                onScroll={Animated.event(
+                    [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+                    { useNativeDriver: true }
+                )}
+                data={pokemons}
+                keyExtractor={(item) => item.num}
+                renderItem={({ item, index }) => {
+                    const cardHeight = 170
+
+                    const inputRange = [
+                        -1,
+                        0,
+                        cardHeight * index,
+                        cardHeight * (index + 2)
+                    ]
+
+                    const scale = scrollY.interpolate({
+                        inputRange,
+                        outputRange: [1, 1, 1, 0] //size at every point in keyframe
+                    })
+
+                    return <Animated.View style={{ transform: [{ scale }] }}>
+                        <StyledPokemonCard number={item.num}
+                            name={item.name}
+                            types={item.type}
+                            image={item.img}
+                            onPress={() => navigation.navigate(Pokemon, { pokemon: item })}
+                        />
+                    </Animated.View>
+                }}
+            />
+            {/* <ScrollView>
                 {
                     pokemons.length > 0 && pokemons.map(pokemon =>
-                        <PokemonCard key={pokemon.id} number={pokemon.num}
+                        <StyledPokemonCard key={pokemon.id} number={pokemon.num}
                             name={pokemon.name}
                             types={pokemon.type}
                             image={pokemon.img}
@@ -37,7 +72,7 @@ const Home = ({ navigation }) => {
                         />
                     )
                 }
-            </ScrollView>
+            </ScrollView> */}
         </StyledHome>
     )
 }
--- src/routes/Home/styles.js
+++ src/routes/Home/styles.js
@@ -3,6 +3,8 @@ import styled from 'styled-components/native'
 import { Colors } from '@styles'
 import Heading from '@components/Heading'
 import Span from '@components/Span'
+import Searchbar from '@components/Searchbar'
+import PokemonCard from '@components/PokemonCard'
 
 //  vertical margin between components
 const vSpacing = 10
@@ -19,18 +21,18 @@ const Description = styled(Span)`
                     color:${Colors.mountainMist};
                     `
 
-const styles = StyleSheet.create({
-    searchbar: {
-        marginVertical: vSpacing
-    },
-    pokemonCard: {
-        marginBottom: vSpacing
-    }
-})
+const StyledSearchbar = styled(Searchbar)`
+                        margin:${vSpacing}px 0;
+                        `
+
+const StyledPokemonCard = styled(PokemonCard)`
+                            margin-bottom:${vSpacing}px;
+                            `
 
 module.exports = {
     StyledHome,
     Title,
     Description,
-    styles
+    StyledSearchbar,
+    StyledPokemonCard
 }
