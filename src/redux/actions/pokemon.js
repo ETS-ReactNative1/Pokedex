a8722b8916b087199dceaeba4f85e5d432c7fa7b
A-amon <64846892+A-amon@users.noreply.github.com>
2021-07-25 11:41:11 +0800

    Changed pagination length to 50

--- src/redux/actions/pokemon.js
+++ src/redux/actions/pokemon.js
@@ -1,7 +1,7 @@
 import { GET_ALL_POKEMONS } from '../actionTypes'
 import pokemons from '@redux/pokemon.json'
 
-export const getAllPokemons = (page = 1, length = 10) => {
+export const getAllPokemons = (page = 1, length = 50) => {
     return dispatch => {
         dispatch({
             type: GET_ALL_POKEMONS,
