80cb07f1bd033e6a214c36f0d09a377610d6aa06
A-amon <64846892+A-amon@users.noreply.github.com>
2021-07-26 00:15:51 +0800

    Separate logic from UI

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -1,23 +1,12 @@
-import React, { useEffect, useRef } from 'react'
-import Animated from 'react-native-reanimated'
-import { useSelector, useDispatch } from 'react-redux'
+import React from 'react'
 import PropTypes from 'prop-types'
-import { FlatList } from 'react-native'
+import Animated from 'react-native-reanimated'
+import useHome from './useHome'
 import { Pokemon } from '@routes'
-import { getAllPokemons } from '@redux/actions/pokemon'
 import { StyledHome, Title, Description, StyledSearchbar, StyledPokemonCard } from './styles'
 
 const Home = ({ navigation }) => {
-    const dispatch = useDispatch()
-    const pokemons = useSelector((state) => state.pokemon.items)
-
-    const scrollY = useRef(new Animated.Value(0)).current
-
-    const AnimatedFlatList = Animated.createAnimatedComponent(FlatList)
-
-    useEffect(() => {
-        dispatch(getAllPokemons())
-    }, [])
+    const { scrollEventHandler, AnimatedFlatList, pokemons, scrollY } = useHome()
 
     return (
         <StyledHome>
@@ -28,38 +17,38 @@ const Home = ({ navigation }) => {
                 Search for Pokemon by name or using the National Pokedex number.
             </Description>
             <StyledSearchbar />
-            <AnimatedFlatList
-                onScroll={Animated.event(
-                    [{ nativeEvent: { contentOffset: { y: scrollY } } }],
-                    { useNativeDriver: true }
-                )}
-                data={pokemons}
-                keyExtractor={(item) => item.num}
-                renderItem={({ item, index }) => {
-                    const cardHeight = 170
+            {
+                pokemons.length > 0 && <AnimatedFlatList
+                    onScroll={scrollEventHandler()}
+                    data={pokemons}
+                    keyExtractor={(item) => item.num}
+                    removeClippedSubviews={true}
+                    renderItem={({ item, index }) => {
+                        const cardHeight = 170
 
-                    const inputRange = [
-                        -1,
-                        0,
-                        cardHeight * index,
-                        cardHeight * (index + 2)
-                    ]
+                        const inputRange = [
+                            -1,
+                            0,
+                            cardHeight * index,
+                            cardHeight * (index + 2)
+                        ]
 
-                    const scale = scrollY.interpolate({
-                        inputRange,
-                        outputRange: [1, 1, 1, 0] //size at every point in keyframe
-                    })
+                        const scale = scrollY.interpolate({
+                            inputRange,
+                            outputRange: [1, 1, 1, 0] //size at every point in keyframe
+                        })
 
-                    return <Animated.View style={{ transform: [{ scale }] }}>
-                        <StyledPokemonCard number={item.num}
-                            name={item.name}
-                            types={item.type}
-                            image={item.img}
-                            onPress={() => navigation.navigate(Pokemon, { pokemon: item })}
-                        />
-                    </Animated.View>
-                }}
-            />
+                        return <Animated.View style={{ transform: [{ scale }] }}>
+                            <StyledPokemonCard number={item.num}
+                                name={item.name}
+                                types={item.type}
+                                image={item.img}
+                                onPress={() => navigation.navigate(Pokemon, { pokemon: item })}
+                            />
+                        </Animated.View>
+                    }}
+                />
+            }
             {/* <ScrollView>
                 {
                     pokemons.length > 0 && pokemons.map(pokemon =>
---
+++ src/routes/Home/useHome.js
@@ -0,0 +1,42 @@
+import { useRef, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useIsMounted } from '@functions/utilities.js'
+import Animated from 'react-native-reanimated'
+import { FlatList } from 'react-native'
+import { getAllPokemons } from '@redux/actions/pokemon'
+
+const useHome = () => {
+    const isMounted = useIsMounted()
+
+    const dispatch = useDispatch()
+    const pokemons = useSelector((state) => state.pokemon.items)
+
+    const scrollY = useRef(new Animated.Value(0)).current
+
+    const AnimatedFlatList = Animated.createAnimatedComponent(FlatList)
+
+    /**
+     * Retrieve pokemons when component is mounted
+     * @return {void}
+     */
+    useEffect(() => {
+        if (isMounted.current)
+            dispatch(getAllPokemons())
+    }, [])
+
+    const scrollEventHandler = () => {
+        return Animated.event(
+            [{ nativeEvent: { contentOffset: { y: scrollY } } }],
+            { useNativeDriver: true }
+        )
+    }
+
+    return {
+        scrollEventHandler,
+        AnimatedFlatList,
+        pokemons,
+        scrollY
+    }
+}
+
+export default useHome
